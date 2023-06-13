44a90a24c8b6f66cac90f112e2c9d603c075931f
Scott Wright <scott.wright.uk@gmail.com>
2023-06-13 14:22:00 +0100

    add: initial files

---
+++ src/index.js
@@ -0,0 +1,13 @@
+
+const pageLoad = () => {
+    const h1 = document.createElement('h1');
+}
+
+/* <h1>Ichiraku Ramen Bar</h1>
+<p>Step into Ichiraku Ramen Bar, where tradition intertwines with modernity to craft an extraordinary ramen experience in the heart of London.
+Immerse yourself in the world of authentic Japanese flavors as we proudly present our commitment to using the finest Japanese ingredients,
+meticulously selected to deliver an unparalleled dining adventure.</p>
+<img src="../src/img/leon-bublitz-fBeRt6DnhC8-unsplash.jpg" alt=" Ramen Bar"></img> */
+
+// Load Page
+pageLoad();
