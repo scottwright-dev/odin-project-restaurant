1e82f71b750c21ae8346caf306e54bd385628d22
Scott Wright <scott.wright.uk@gmail.com>
2023-06-14 17:34:09 +0100

    add: initial structure for menu page
    This commit creates a menuContainer element, which will serve as a placeholder for the menu items.
    The text content of the menuContainer is provisionally set to "Menu items to be added here". Actual menu items will be added later, I intend to simply establish the basic functionality first.

---
+++ src/menuPage.js
@@ -0,0 +1,8 @@
+export function createMenuPage(parentElement) {
+    
+    const menuContainer = document.createElement('div');
+    menuContainer.textContent = 'Menu items to be added here';
+  
+    parentElement.appendChild(menuContainer);
+  }
+  
