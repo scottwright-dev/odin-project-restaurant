1b987c072f5b327ccf76a44af2ed911b9577b429
Scott Wright <scott.wright.uk@gmail.com>
2023-06-14 17:54:02 +0100

    feat: implement tab switching function
    This commit establishes the tab switching functionality and sets up the initial loading of the restaurant site.
    The `switchTab` function clears the existing content and displays the appropriate tab content based on the selected tab.
    Refactor the `initialLoad` function to call the `switchTab` function with the 'home' tab to display the home page upon initial load.
    
    Event listeners to be added in the next commit.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,31 @@
 import { createHomePage } from './homePage';
+import { createMenuPage } from './menuPage';
 
+const homeTab = document.querySelector('#home-tab');
+const menuTab = document.querySelector('#menu-tab');
+
+// Tab switching
+function switchTab(tab) {
+  const contentDiv = document.querySelector('#content');
+  contentDiv.textContent = '';
+
+  switch (tab) {
+    case 'home':
+      createHomePage(contentDiv);
+      break;
+    case 'menu':
+      createMenuPage(contentDiv);
+      break;
+    default:
+      break;
+  }
+}
+
+// Initial loading
 function initialLoad() {
   createHomePage();
+  switchTab('home'); // Display home page initially
 }
 
 initialLoad();
+
