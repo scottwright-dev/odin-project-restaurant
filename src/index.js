85da33b9be87680c9562c7793e7469d58e7b405e
Scott Wright <scott.wright.uk@gmail.com>
2023-06-14 09:40:42 +0100

    refactor: update index.js file for initial setup
    This commit adds the initial setup for the project. The `index.js` file imports the `createHomePage` function from the `homePage.js` module and defines an `initialLoad` function.
    The 'initialLoad' function calls the `createHomePage` function. This is to set up the basic structure to dynamically generate the content of the homepage.
    
    Next step will be to implement the `createHomePage` function inside the `homePage.js` file to generate the actual content.

--- src/index.js
+++ src/index.js
@@ -1,13 +1,7 @@
+import { createHomePage } from './homePage';
 
-const pageLoad = () => {
-    const h1 = document.createElement('h1');
+function initialLoad() {
+  createHomePage();
 }
 
-/* <h1>Ichiraku Ramen Bar</h1>
-<p>Step into Ichiraku Ramen Bar, where tradition intertwines with modernity to craft an extraordinary ramen experience in the heart of London.
-Immerse yourself in the world of authentic Japanese flavors as we proudly present our commitment to using the finest Japanese ingredients,
-meticulously selected to deliver an unparalleled dining adventure.</p>
-<img src="../src/img/leon-bublitz-fBeRt6DnhC8-unsplash.jpg" alt=" Ramen Bar"></img> */
-
-// Load Page
-pageLoad();
+initialLoad();
