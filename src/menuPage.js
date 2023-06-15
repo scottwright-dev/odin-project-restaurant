81d236a024535dc653948b83165f7d4fa627c9e3
Scott Wright <scott.wright.uk@gmail.com>
2023-06-15 10:55:32 +0100

    feat: add tab switching functionality to menu page
    This commit adds a "Home" tab to the menu page that triggers the switchTab function with the argument 'home' when clicked.
    This changes allow the user to navigate back to the home page from the menu page.

--- src/menuPage.js
+++ src/menuPage.js
@@ -1,8 +1,17 @@
-export function createMenuPage(parentElement) {
-    
+export function createMenuPage(parentElement, switchTab) {
+    const content = document.querySelector('#content');
+  
+    // home tab
+    const home = document.createElement('a');
+    home.textContent = 'Home';
+    home.href = '#';
+    home.addEventListener('click', () => switchTab('home'));
+    content.appendChild(home);
+
+    // menu content
     const menuContainer = document.createElement('div');
     menuContainer.textContent = 'Menu items to be added here';
-  
+    
     parentElement.appendChild(menuContainer);
   }
   
