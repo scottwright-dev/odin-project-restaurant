d23e346c07bd9d0b70a161dfed3568899c147442
Scott Wright <scott.wright.uk@gmail.com>
2023-06-16 10:03:58 +0100

    refactor: menuPage function, implement tabManager
    This commit:
    - Imports createTabs function from tabManager module.
    - Implements createTabs function to create tabs using the switchTab function.
    - Removes creation of the home tab from within the createMenuPage function.

--- src/menuPage.js
+++ src/menuPage.js
@@ -1,17 +1,11 @@
-export function createMenuPage(parentElement, switchTab) {
-    const content = document.querySelector('#content');
-  
-    // home tab
-    const home = document.createElement('a');
-    home.textContent = 'Home';
-    home.href = '#';
-    home.addEventListener('click', () => switchTab('home'));
-    content.appendChild(home);
+import { createTabs } from './tabManager';
 
-    // menu content
-    const menuContainer = document.createElement('div');
-    menuContainer.textContent = 'Menu items to be added here';
-    
-    parentElement.appendChild(menuContainer);
-  }
-  
+export function createMenuPage(parentElement, switchTab) {
+  createTabs(switchTab);
+
+  // menu content
+  const menuContainer = document.createElement('div');
+  menuContainer.textContent = 'Menu items to be added here';
+
+  parentElement.appendChild(menuContainer);
+}
