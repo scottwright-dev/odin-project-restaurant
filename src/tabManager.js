4c359cbb5c49779fb470f5b08b2019a9a53938f7
Scott Wright <scott.wright.uk@gmail.com>
2023-06-16 11:02:26 +0100

    refactor: createTabs function & update export
    This commit changes the createTabs function to use the default export syntax, it was previously exported as a named export.
    This change improves code readability by moving the export to the bottom of the function which makes it easier to see.
    It also helps adhere to the pattern of using default exports for single primary functions.

--- src/tabManager.js
+++ src/tabManager.js
@@ -1,20 +1,21 @@
-export function createTabs(switchTab) {
-    const content = document.querySelector('#content');
-  
-    function createIndividualTab(label, tabName) {
-      const tab = document.createElement('a');
-      tab.textContent = label;
-      tab.href = '#';
-      tab.addEventListener('click', () => switchTab(tabName));
-      return tab;
-    }
-  
-    const homeTab = createIndividualTab('Home', 'home');
-    const menuTab = createIndividualTab('Menu', 'menu');
-    const contactTab = createIndividualTab('Contact', 'contact');
-  
-    content.appendChild(homeTab);
-    content.appendChild(menuTab);
-    content.appendChild(contactTab);
+function createTabs(switchTab) {
+  const content = document.querySelector('#content');
+
+  function createIndividualTab(label, tabName) {
+    const tab = document.createElement('a');
+    tab.textContent = label;
+    tab.href = '#';
+    tab.addEventListener('click', () => switchTab(tabName));
+    return tab;
   }
-  
+
+  const homeTab = createIndividualTab('Home', 'home');
+  const menuTab = createIndividualTab('Menu', 'menu');
+  const contactTab = createIndividualTab('Contact', 'contact');
+
+  content.appendChild(homeTab);
+  content.appendChild(menuTab);
+  content.appendChild(contactTab);
+}
+
+export default createTabs;
