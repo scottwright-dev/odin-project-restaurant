4b2f76484b648f6e60cae9408af75d604977d076
Scott Wright <scott.wright.uk@gmail.com>
2023-06-16 10:13:30 +0100

    refactor: code order
    For continuity purposes I have moved createTabs(switchTab); to the top of the function to replicate structure on the other pages.
    Bonus side effect is this puts the navigation at the top of the page. #winning

--- src/homePage.js
+++ src/homePage.js
@@ -2,6 +2,8 @@ import { createTabs } from './tabManager';
 
 export function createHomePage (switchTab) {
     const content = document.querySelector('#content');
+
+    createTabs(switchTab);
    
     // create elements
     const image = document.createElement('img');
@@ -16,7 +18,4 @@ export function createHomePage (switchTab) {
     const copy = document.createElement('p');
     copy.textContent = 'Step into Ichiraku Ramen Bar, where Japanese tradition intertwines with modernity to craft an extraordinary ramen experience in the heart of London.';
     content.appendChild(copy);
-
-    createTabs(switchTab);
-
 }
