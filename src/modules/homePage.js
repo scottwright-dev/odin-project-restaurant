e2d532b8f352074cd4fda694ae3886ebf2e21ebb
Scott Wright <scott.wright.uk@gmail.com>
2023-06-19 15:23:18 +0100

    refactor: shorten copy text on homepage

--- src/modules/homePage.js
+++ src/modules/homePage.js
@@ -18,7 +18,7 @@ export function createHomePage (switchTab) {
     content.appendChild(title);
 
     const copy = document.createElement('p');
-    copy.textContent = 'Step into Ichiraku Ramen Bar, where Japanese tradition intertwines with modernity to craft an extraordinary ramen experience in the heart of London.';
+    copy.textContent = 'Japanese tradition intertwined with modernity, crafting an extraordinary ramen experience in the heart of London.';
     copy.classList.add('homepage-copy');
     content.appendChild(copy);
 }
